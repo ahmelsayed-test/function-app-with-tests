e6ed48db78a320886654b5d63c2c80a3fe46b388
Ahmed ElSayed <ahmels@microsoft.com>
2018-02-08 16:32:36 -0800

    Initial

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        [Fact]
+        public void Test()
+        {
+            FunctionApp27.Function1.Run(null, null).Wait();
+        }
+    }
+}
